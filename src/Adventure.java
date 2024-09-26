cbaa953c709e487e59d3ac95ed55a8b17a727595
CamillaEva <caha0007@stud.kea.dk>
2024-09-26 13:49:50 +0100

    changes

--- src/Adventure.java
+++ src/Adventure.java
@@ -9,33 +9,35 @@ public class Adventure {
     public void buildAdventure() {
 
 //        room objects 1 through 9
-//        currentRoom = new Room("1", "description of room 1");
-//        Room room2 = new Room("2", "description of room 2");
-//        Room room3 = new Room("3", "description of room 3");
-//        Room room4 = new Room("4", "description of room 4");
-//        Room room5 = new Room("5", "description of room 5");
-//        Room room6 = new Room("6", "description of room 6");
-//        Room room7 = new Room("7", "description of room 7");
-//        Room room8 = new Room("8", "description of room 8");
-//        Room room9 = new Room("9", "description of room 9");
+        currentRoom = new Room("1", "description of room 1");
+        Room room2 = new Room("2", "description of room 2");
+        Room room3 = new Room("3", "description of room 3");
+        Room room4 = new Room("4", "description of room 4");
+        Room room5 = new Room("5", "description of room 5");
+        Room room6 = new Room("6", "description of room 6");
+        Room room7 = new Room("7", "description of room 7");
+        Room room8 = new Room("8", "description of room 8");
+        Room room9 = new Room("9", "description of room 9");
 
-        currentRoom = new Room("The Magic Forest:", "\nyou find yourself in the magic forest, its bright green and very vibrant\n" +
-                "you can hear some people talk, somewhere near by, but you can't see anybody.");
-        Room room2 = new Room("Mr. Rabbits house:", "\nyou stand in a pretty little house where everything is cute and cozy. \n" +
-                "it smells like freshbaked cookies, everything is so idyllic");
-        Room room3 = new Room("The Walrus' dinnertable:", "\nyou find yourself sitting at the dinner table of the Walrus, \n" +
-                "all around it are small oyster babies ready to get some promised delicious food, but what they don't know i that the walrus is about to eat them all.");
-        Room room4 = new Room("The March Hare's Tea Party", "\nyou now stand in a room where a March Hare and a man with a very big and wierd hat sits, \nsurrounded" +
-                "by thousands of teacups and big cakes. \nthey are singing some kind of wierd birthday song but like an non-birthday song.");
-        Room room5 = new Room("the Room:", "\nyou are now in a teeny tiny itty bitty room, with only one door, a pillar with a jar of cookies and some kind of potion.");
-        Room room6 = new Room("the Catarpillars':", "\nnow you are standing between the biggest funghis you ever saw in your entire life, on top of one of them \n" +
-                "is a catarpiller even bigger than you. it's smoking a hookah and singing a curious song.");
-        Room room7 = new Room("the Dark Forest:", "\nsuddenly everything is all dark, and creepy, you are standing in a forest.\n" +
-                "it feels like theres eyes watching you from everywhere and you can hear a mischievous laugh, from the top of the tree in front of you");
-        Room room8 = new Room("the Singing Flowers:", "\nwow, what's going on?\nyou're standing in the middle of a flower bed with a choir of flowers singing in perfect harmony,\n" +
-                "you try to sing along but the choir leader is not having any of it.");
-        Room room9 = new Room("the Cardhouse:", "\nno wait, it's not a house, that's a castle. made of cards?" +
-                "you wonder who lives such a place...");
+
+//        //room objects with description and name, to use when personify game.
+//        currentRoom = new Room("The Magic Forest:", "\nyou find yourself in the magic forest, its bright green and very vibrant\n" +
+//                "you can hear some people talk, somewhere near by, but you can't see anybody.");
+//        Room room2 = new Room("Mr. Rabbits house:", "\nyou stand in a pretty little house where everything is cute and cozy. \n" +
+//                "it smells like freshbaked cookies, everything is so idyllic");
+//        Room room3 = new Room("The Walrus' dinnertable:", "\nyou find yourself sitting at the dinner table of the Walrus, \n" +
+//                "all around it are small oyster babies ready to get some promised delicious food, but what they don't know i that the walrus is about to eat them all.");
+//        Room room4 = new Room("The March Hare's Tea Party", "\nyou now stand in a room where a March Hare and a man with a very big and wierd hat sits, \nsurrounded" +
+//                "by thousands of teacups and big cakes. \nthey are singing some kind of wierd birthday song but like an non-birthday song.");
+//        Room room5 = new Room("the Room:", "\nyou are now in a teeny tiny itty bitty room, with only one door, a pillar with a jar of cookies and some kind of potion.");
+//        Room room6 = new Room("the Catarpillars':", "\nnow you are standing between the biggest funghis you ever saw in your entire life, on top of one of them \n" +
+//                "is a catarpiller even bigger than you. it's smoking a hookah and singing a curious song.");
+//        Room room7 = new Room("the Dark Forest:", "\nsuddenly everything is all dark, and creepy, you are standing in a forest.\n" +
+//                "it feels like theres eyes watching you from everywhere and you can hear a mischievous laugh, from the top of the tree in front of you");
+//        Room room8 = new Room("the Singing Flowers:", "\nwow, what's going on?\nyou're standing in the middle of a flower bed with a choir of flowers singing in perfect harmony,\n" +
+//                "you try to sing along but the choir leader is not having any of it.");
+//        Room room9 = new Room("the Cardhouse:", "\nno wait, it's not a house, that's a castle. made of cards?" +
+//                "you wonder who lives such a place...");
 
 
         //room1 adjacent rooms
